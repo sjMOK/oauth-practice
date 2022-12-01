b3535dfce7ba1442d2817e5ec81685687ef8f245
sjMOK <mokseungju@naver.com>
2022-12-01 11:25:29 +0900

    chore: Make 'common' app in django project

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user.apps.UserConfig',
+    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
